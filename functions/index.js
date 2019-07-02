4637f836d7cd113fe0adc106c76fb67704b6a1ae
nonylene <nonylene@gmail.com>
2019-07-03 05:31:00 +0900

    read and write phoneNumbers

--- functions/index.js
+++ functions/index.js
@@ -1,62 +1,75 @@
-// See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
-// for Dialogflow fulfillment library docs, samples, and to report issues
-'use strict';
+'use strict'
 
-const functions = require('firebase-functions');
-const {WebhookClient} = require('dialogflow-fulfillment');
-const {Card, Suggestion} = require('dialogflow-fulfillment');
+const functions = require('firebase-functions')
+const {dialogflow} = require('actions-on-google')
 
-process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+process.env.DEBUG = 'dialogflow:debug' // enables lib debugging statements
 
-exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
-  const agent = new WebhookClient({ request, response });
-  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
-  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+const denyGuest = (conv) => {
+  console.log(conv.user.verification)
+  if (conv.user.verification !== 'VERIFIED') {
+    // XXX
+    conv.close('ゲストには対応していません。')
+    return true
+  } 
+  return false
+}
 
-  function welcome(agent) {
-    agent.add(`Welcome to my agent!`);
+const personRequest = (conv, params) => {
+  if (denyGuest(conv)) return
+
+  // Initialize
+  if (!('phoneNumbers' in conv.user.storage)) {
+    conv.user.storage.phoneNumbers = {}
   }
 
-  function fallback(agent) {
-    agent.add(`I didn't understand`);
-    agent.add(`I'm sorry, can you try again?`);
+  const name = params.name.name
+  conv.data.name = name
+
+  const phoneNumber = conv.user.storage.phoneNumbers[name]
+  if (!phoneNumber) {
+    conv.ask(`番号が見つかりませんでした。${name}さんの番号は何ですか？`)
+  } else {
+    conv.data.phoneNumber = phoneNumber
+    conv.contexts.set('phone-call', 1)
+    conv.ask(`${name}さんの${phoneNumber}を呼び出します。よろしいですか？`)
+  }
+}
+
+const phoneNumberRequest = (conv, params) => {
+  if (denyGuest(conv)) return
+
+  const phoneNumber = params['phone-number']
+  conv.data.phoneNumber = phoneNumber
+  if (conv.data.name) {
+    conv.user.storage.phoneNumbers[conv.data.name] = phoneNumber
+    conv.add(`${conv.data.name}さんの番号を${phoneNumber}と登録しました。`)
   }
 
-  // // Uncomment and edit to make your own intent handler
-  // // uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
-  // // below to get this function to be run when a Dialogflow intent is matched
-  // function yourFunctionHandler(agent) {
-  //   agent.add(`This message is from Dialogflow's Cloud Functions for Firebase editor!`);
-  //   agent.add(new Card({
-  //       title: `Title: this is a card title`,
-  //       imageUrl: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
-  //       text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
-  //       buttonText: 'This is a button',
-  //       buttonUrl: 'https://assistant.google.com/'
-  //     })
-  //   );
-  //   agent.add(new Suggestion(`Quick Reply`));
-  //   agent.add(new Suggestion(`Suggestion`));
-  //   agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
-  // }
+  conv.contexts.set('phone-call', 1)
+  conv.ask(`${phoneNumber}を呼び出します。よろしいですか？`)
+}
 
-  // // Uncomment and edit to make your own Google Assistant intent handler
-  // // uncomment `intentMap.set('your intent name here', googleAssistantHandler);`
-  // // below to get this function to be run when a Dialogflow intent is matched
-  // function googleAssistantHandler(agent) {
-  //   let conv = agent.conv(); // Get Actions on Google library conv instance
-  //   conv.ask('Hello from the Actions on Google client library!') // Use Actions on Google library
-  //   agent.add(conv); // Add Actions on Google library responses to your agent's response
-  // }
-  // // See https://github.com/dialogflow/dialogflow-fulfillment-nodejs/tree/master/samples/actions-on-google
-  // // for a complete Dialogflow fulfillment library Actions on Google client library v2 integration sample
+const phoneCallAccept = (conv) => {
+  if (denyGuest(conv)) return
 
-  // Run the proper function handler based on the matched Dialogflow intent name
-  let intentMap = new Map();
-  intentMap.set('Default Welcome Intent', welcome);
-  intentMap.set('Default Fallback Intent', fallback);
-  // intentMap.set('your intent name here', yourFunctionHandler);
-  // intentMap.set('your intent name here', googleAssistantHandler);
-  agent.handleRequest(intentMap);
-});
+  conv.ask(`${conv.data.phoneNumber}を呼び出します。`)
+}
 
+const deleteUserStorage = (conv) => {
+  if (denyGuest(conv)) return
+
+  conv.user.storage = {}
+  conv.ask('データを削除しました。')
+}
+
+const app = dialogflow({
+  debug: true,
+})
+
+app.intent('person_request', personRequest)
+app.intent('phone-number_request', phoneNumberRequest)
+app.intent('phone-call_accept', phoneCallAccept)
+app.intent('delete_user-storage', deleteUserStorage)
+
+exports.dialogflowLostFound = functions.https.onRequest(app)
